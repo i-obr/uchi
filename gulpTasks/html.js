29d48b1d10d644512185d16dc38b073f91fafea3
i-obr <obr.iv@ya.ru>
2017-07-31 09:37:52 +0300

    nunjacks

---
+++ gulpTasks/html.js
@@ -0,0 +1,7 @@
+import gulp           from 'gulp';
+import nunjucksRender    from 'gulp-nunjucks-render';
+import plumber           from 'gulp-plumber';
+import gulpif            from 'gulp-if';
+import changed           from 'gulp-changed';
+import prettify          from 'gulp-prettify';
+import frontMatter       from 'gulp-front-matter';
