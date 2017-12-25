092e726fe97f2da173f6cd4116bdd8b1e3ff01f4
i-obr <obr.iv@ya.ru>
2017-12-25 13:03:45 +0300

    Update 2017-12-25T10:03:40.945Z

--- gulpTasks/clean.js
+++
@@ -1,4 +0,0 @@
-import gulp from 'gulp';
-import del  from 'del';
-
-gulp.task('clean', () => del('build'));
--- gulpTasks/deploy.js
+++
@@ -1,7 +0,0 @@
-import gulp     from 'gulp';
-import ghPages  from 'gulp-gh-pages';
-
-gulp.task('deploy', () => {
-  return gulp.src('./build/**/*')
-    .pipe(ghPages());
-});
